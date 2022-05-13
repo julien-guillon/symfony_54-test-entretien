a926b725cc6a9e32622c3d1c639281fcf363f773
Damien Riolet <damienriolet@outlook.fr>
2022-05-13 17:59:20 +0200

    Article Controller created

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/articles', name: 'app_articles')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+        return $this->render('article/index.html.twig', compact('articles'));
+    }
+}
