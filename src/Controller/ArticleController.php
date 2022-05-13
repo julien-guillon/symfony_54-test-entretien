486f53a55081a1e9e607de8679cb14ee4eb51510
Damien Riolet <damienriolet@outlook.fr>
2022-05-13 22:05:01 +0200

    Create and Update form for Articles are now added. Routes in /articles/ are also added

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,100 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\AddArticleType;
+use App\Form\UpdateArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/articles', name: 'app_articles')]
+    #[Route('/articles', name: 'app_articles', methods:"GET")]
     public function index(ArticleRepository $articleRepository): Response
     {
         $articles = $articleRepository->findAll();
         return $this->render('article/index.html.twig', compact('articles'));
+    }
+
+    #[Route('/article/{id<[0-9]+>}', name: 'app_articles_show', methods:"GET")]
+    public function show(Article $art): Response
+    {
+        return $this->render('article/showArticle.html.twig', compact('art'));
+    }
+
+    #[Route('/articles/add', name: 'app_articles_add', methods:"GET|POST")]
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    {
+        $art = new Article;
+        $form = $this->createForm(AddArticleType::class, $art);
+            
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $photoItem = $form->get('photo')->getData(); 
+
+            if($photoItem) {
+                $filename = pathinfo($photoItem->getClientOriginalName(), PATHINFO_FILENAME);
+                $slug_filename = $slugger->slug($filename);
+                $photo_filename = $slug_filename.'-'.uniqid().'.'.$photoItem->guessExtension();
+        
+                try {
+                    $photoItem->move(
+                        $this->getParameter('upload_img_directory'),
+                        $photo_filename
+                    );
+                } catch (FileException $e) {
+                    return $this->json([
+                        'error' => "Le fichier n'a pas été déplacé",
+                    ]);
+                }
+
+                $art->setPhoto($photo_filename);
+            }
+            
+            $em->persist($art);
+            $em->flush();
+            return $this->redirectToRoute('app_articles');
+
+        }
+
+        return $this->render('article/addArticle.html.twig', [
+            'addArticleForm' => $form->createView()
+        ]);
+    }
+
+    #[Route('/article/update/{id<[0-9]+>}', name: 'app_articles_update')]
+    public function update(Article $art, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(UpdateArticleType::class, $art);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            
+            return $this->redirectToRoute('app_articles');
+        }
+
+        return $this->render('article/updateArticle.html.twig', [
+            'art' => $art,
+            'updateArticleForm' => $form->createView()
+        ]);
+    }
+
+
+    #[Route('/article/delete/{id<[0-9]+>}', name: 'app_articles_delete')]
+    public function delete(Article $art): Response
+    {
+        return $this->json([
+            'status' => 'post deleted!',
+        ]);
     }
 }
---
+++ src/Form/AddArticleType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class AddArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('introduction')
+            ->add('content')
+            ->add('photo', FileType::class, [
+                'label' => 'Photo',
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '4028k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a png or jpg or webp image',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
