0eee7c407f45d4fcd1e42d147ac24d62bf152702
Damien Riolet <damienriolet@outlook.fr>
2022-05-14 19:57:58 +0200

    Form responsive styles

--- src/Form/AddArticleType.php
+++ src/Form/AddArticleType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Article;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
@@ -14,11 +17,19 @@ class AddArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('introduction')
-            ->add('content')
+            ->add('title', TextType::class, [
+                "label" => "Titre de l'article"
+            ])
+            ->add('introduction', TextType::class, [
+                "label" => "Introduction (optionnel)",
+                'required' => false,
+            ])
+            ->add('content', TextareaType::class, [
+                "label" => "Contenu textuel"
+            ])
             ->add('photo', FileType::class, [
-                'label' => 'Photo',
+                'label' => "Image de l'article (optionnel)",
+                'attr' => ['class' => 'form__inputFile'],
                 'required' => false,
                 'constraints' => [
                     new File([
@@ -28,7 +39,7 @@ class AddArticleType extends AbstractType
                             'image/png',
                             'image/webp',
                         ],
-                        'mimeTypesMessage' => 'Please upload a png or jpg or webp image',
+                        'mimeTypesMessage' => 'Utilisez s\'il vous plait un fichier png, jpg ou webp',
                     ])
                 ],
             ])
