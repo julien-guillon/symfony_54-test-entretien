75d13c458ac47a3746434eb5edb733fb43e6efb6
Damien Riolet <damienriolet@outlook.fr>
2022-05-16 10:02:41 +0200

    Solving template issues and styling sticky navbar for easier navigation

--- src/Form/UpdateArticleType.php
+++ src/Form/UpdateArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,13 @@ class UpdateArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('introduction')
-            ->add('content')
+            ->add('introduction', TextType::class, [
+                "label" => "Introduction (optionnel)",
+                'required' => false,
+            ])
+            ->add('content', TextareaType::class, [
+                "label" => "Contenu textuel"
+            ])
         ;
     }
 
